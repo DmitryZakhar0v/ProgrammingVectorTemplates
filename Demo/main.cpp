e2624972d296f15cadf7853fd089c343a9d638c7
DmitryZakhar0v <pcixtimessquare@gmail.com>
2023-10-17 18:37:00 +0300

    Темплеты, но не работают операторы

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -6,11 +6,13 @@ int main()
 {
 	try
 	{
+		
+		
 		Vector vector_1{ 12, 33,44,55,11 };
 		Vector vector_2{ 33, 22 ,44, 55 };
-		std::cout << vector_1 << std::endl;
+		
 		vector_1[1] = 10;
-		std::cout << vector_1 << std::endl;
+		//std::cout << vector_1 << std::endl;
 	}
 	catch (const std::exception& err)
 	{
--- Vector/Vector.h
+++ Vector/Vector.h
@@ -1,9 +1,10 @@
 ﻿#pragma once
 #include <iostream>
+#include <sstream>
 
 namespace miit::vector
 {
-
+	template<typename T>
 	class Vector final
 	{
 	public:
@@ -12,7 +13,7 @@ namespace miit::vector
 		@brief Создание vector'a по initializer_list
 		@param list Лист по которому строится vector
 		*/
-		Vector(const std::initializer_list<int> list);
+		Vector(const std::initializer_list<T> list);
 
 		/**
 		@brief Чистит памать за vector
@@ -29,7 +30,7 @@ namespace miit::vector
 		@brief Добавление элемента в вектор
 		@param Значение элемента
 		*/
-		void append(int value);
+		void append(T value);
 
 		/**
 		@brief Удаление элемента из vector по индексу
@@ -42,7 +43,7 @@ namespace miit::vector
 		@param value Искомое значение
 		@return индекс элемента если он есть в vector и -1 если его в нем нет
 		*/
-		int find(int value) const;
+		int find(T value) const;
 
 		/**
 		@brief Получение информации об отсутствии элементов в vector
@@ -55,40 +56,40 @@ namespace miit::vector
 		@param index Индекс элемента в векторе
 		@return Элемент из вектора
 		*/
-		int& operator[](int index);
+		T& operator[](int index);
 
 		/**
 		@brief Перегрузка оператора копирования
 		@param Vector Вектор для копирования
 		@return Скопированный объект типа Vector
 		*/
-		Vector& operator=(const Vector& vector);
+		Vector<T>& operator=(const Vector<T>& vector);
 
 		/*
 		@brief Перегрузка move оператора
 		@param Vector Список для мува
 		@return Объект типа DLList
 		*/
-		Vector& operator=(Vector&& vector) noexcept;
+		Vector<T>& operator=(Vector<T>&& vector) noexcept;
 
 		/**
 		@brief Move конструктор
 		@param vector Вектор для мува
 		*/
-		Vector(const Vector& vector);
+		Vector(const Vector<T>& vector);
 
 		/**
 		@brief Конструктор копирования
 		@param vector Вектор для копирования
 		*/
-		Vector(Vector&& vector) noexcept;
+		Vector(Vector<T>&& vector) noexcept;
 
 		/*
 		@brief Перегрузка оператора вывода
 		@param os Поток вывода
 		@param vector Вектор для вывода
 		*/
-		friend std::ostream& operator<<(std::ostream& os, Vector& vector) noexcept;
+		friend std::ostream& operator<<(std::ostream& os, Vector<T>& vector) noexcept;
 
 		/**
 		@brief Перегрузка оператора равно
@@ -96,7 +97,7 @@ namespace miit::vector
 		@param lha Второй аргумент для сравнения
 		@return false если равны и true если нет
 		*/
-		friend bool operator==(const Vector& rha, const Vector& lha) noexcept;
+		friend bool operator==(const Vector<T>& rha, const Vector<T>& lha) noexcept;
 
 		/**
 		@brief Перегрузка оператора не равно
@@ -104,7 +105,7 @@ namespace miit::vector
 		@param lha Второй аргумент для сравнения
 		@return true если равны и false если нет
 		*/
-		friend bool operator!=(const Vector& rha, const Vector& lha) noexcept;
+		friend bool operator!=(const Vector<T>& rha, const Vector<T>& lha) noexcept;
 
 		/**
 		@brief Преобразование объета в строку
@@ -114,7 +115,193 @@ namespace miit::vector
 
 	private:
 		size_t size;
-		int* values;
+		T* values;
 
 	};
+	
+	template<typename T>
+	inline Vector<T>::Vector(const std::initializer_list<T> list)
+		:size(0)
+	{
+		for (int value : list)
+		{
+			this->append(value);
+		}
+	}
+
+	template<typename T>
+	inline Vector<T>::~Vector()
+	{
+		delete[] this->values;
+		this->values = nullptr;
+	}
+
+	template<typename T>
+	inline int Vector<T>::get_size() const
+	{
+		return this->size;
+	}
+
+	template<typename T>
+	void Vector<T>::append(T value)
+	{
+		int* new_values = new int[this->size + 1];
+		for (size_t i = 0; i < this->size; i++)
+		{
+			new_values[i] = this->values[i];
+		}
+		new_values[this->size] = value;
+		delete[] this->values;
+		this->values = new_values;
+		this->size++;
+	}
+
+
+	template<typename T>
+	bool Vector<T>::is_empty() const
+	{
+		return (this->size == 0);
+	}
+
+	
+	template<typename T>
+	Vector<T>& Vector<T>::operator=(Vector<T>&& vector) noexcept
+	{
+		std::swap(vector.values, this->values);
+		std::swap(vector.size, this->size);
+		return *this;
+	}
+
+	template<typename T>
+	Vector<T>::Vector(const Vector<T>& vector)
+		:size(0), values(nullptr)
+	{
+		if (!this->is_empty())
+		{
+			delete[] this->values;
+			this->values = nullptr;
+			this->size = 0;
+		}
+		for (int i = 0; i < vector.get_size(); i++)
+		{
+			this->append(vector.values[i]);
+		}
+
+	}
+
+	template<typename T>
+	Vector<T>::Vector(Vector<T>&& vector) noexcept
+		:size(0), values(nullptr)
+	{
+		std::swap(vector.values, this->values);
+		std::swap(vector.size, this->size);
+	}
+
+	template<typename T>
+	T& Vector<T>::operator[](int index)
+	{
+		if (this->is_empty() || index >= this->size or index < 0)
+		{
+			throw std::out_of_range("Incorrect Index");
+		}
+		return this->values[index];
+	}
+
+	template<typename T>
+	inline Vector<T>& Vector<T>::operator=(const Vector& vector)
+	{
+		if (!this->is_empty())
+		{
+			delete[] this->values;
+			this->values = nullptr;
+			this->size = 0;
+		}
+		for (int i = 0; i < vector.get_size(); i++)
+		{
+			this->append(vector.values[i]);
+		}
+		return *this;
+	}
+
+	
+	template<typename T>
+	void Vector<T>::pop(int index)
+	{
+		if (this->size == 0 or index < 0 or index >= this->size)
+		{
+			throw std::out_of_range("Incorrect Index");
+		}
+		int* vector_1 = new int[this->size - 1];
+		size_t current = 0;
+		for (size_t i = 0; i < this->size; i++)
+		{
+			if (i != index)
+			{
+				vector_1[current] = this->values[i];
+				current++;
+			}
+		}
+		delete[] this->values;
+		this->values = vector_1;
+		this->size--;
+	}
+
+	template<typename T>
+	int Vector<T>::find(T value) const
+	{
+		if (this->is_empty())
+		{
+			return -1;
+		}
+
+		int index = 0;
+		while ((index < this->size) && this->values[index] != value)
+		{
+			index++;
+		}
+		if (index == this->size)
+		{
+			return -1;
+		}
+		else
+		{
+			return index;
+		}
+	}
+
+
+	template<typename T>
+	std::string Vector<T>::to_string() const noexcept
+	{
+		std::stringstream result;
+		result << "[ ";
+		for (size_t i = 0; i < this->size; i++)
+		{
+			result << this->values[i] << " ";
+		}
+		result << "]";
+		return result.str();
+	}
+
+	template<typename T>
+	std::ostream& operator<<(std::ostream& os, Vector<T>& vector) noexcept
+	{
+		return os << vector.to_string();
+	}
+
+	template<typename T>
+	bool operator==(const Vector<T>& rha, const Vector<T>& lha) noexcept
+	{
+		return (rha.to_string() == lha.to_string());
+	}
+
+	template<typename T>
+	bool operator!=(const Vector<T>& rha, const Vector<T>& lha) noexcept
+	{
+		return !(rha == lha);
+	}
+
+
 };
+
+
